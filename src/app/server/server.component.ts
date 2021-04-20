545bb6fd8434c443c384718086bf1c0f1f30eb35
KarlokCheung <georgecheung07@gmail.com>
2021-04-20 23:29:33 +1000

    add styles in warning and success component

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html'
 })
-export class ServerComponent {
+export class ServerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
